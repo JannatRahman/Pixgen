d7247c7db9db038294dc4f421d17399a0d0bf6d0
JannatRahman <you@example.com>
2026-04-29 20:26:19 +0600

    all photos section done

---
+++ src/app/all-photos/page.jsx
@@ -0,0 +1,22 @@
+import PhotoCard from "@/components/PhotoCard";
+
+
+const AllPhotosPage = async () => {
+  const res = await fetch("https://pixgen-black-delta.vercel.app/data.json");
+  const photos = await res.json();
+  return (
+    <div>
+      <h1 className="text-2xl font-bold m-4">All Photos</h1>
+
+      <div className="grid grid-cols-4 gap-4">
+        {
+          photos.map(photo => 
+            <PhotoCard key={photo.id} photo={photo}/>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default AllPhotosPage;
