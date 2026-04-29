59e50d91666f8235cbc3bcb6a7b05c925f7d3081
JannatRahman <you@example.com>
2026-04-29 20:06:06 +0600

    home page design done

---
+++ src/components/PhotoCard.jsx
@@ -0,0 +1,47 @@
+import { Button, Card, Chip, Separator } from '@heroui/react';
+import Image from 'next/image';
+import { BiDownload } from 'react-icons/bi';
+import { FaHeart } from 'react-icons/fa';
+
+
+const PhotoCard = ({photo}) => {
+  return (
+    <Card className='border rounded-xl'>
+      <div className='relative w-full aspect-square'> 
+        <Image 
+        src={photo.imageUrl}
+        sizes='"(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"'
+        fill
+        alt={photo.title}
+        className='rounded-xl object-cover'
+        />
+        <Chip size='sm' className='absolute right-2 top-2'>{photo.category}</Chip>
+      </div>
+      <div>
+        
+        <h2 className='font-semibold'>{photo.title}</h2>
+      </div>
+
+     <div className='flex gap-10 items-center'>
+     
+      <div className='flex gap-1 items-center pl-6'>
+        <p><FaHeart className='text-red-600'
+         /></p>
+        <p>{photo.likes}</p>
+        </div>
+ 
+         <Separator orientation='vertical'/>
+
+      <div className='flex gap-1 items-center '>
+        <p><BiDownload 
+         /></p>
+        <p>{photo.downloads}</p>
+        </div>
+
+     </div>
+     <Button variant='outline' className={'w-full'}>View</Button>
+    </Card>
+  );
+};
+
+export default PhotoCard;
---
+++ src/components/TopGenerations.jsx
@@ -0,0 +1,24 @@
+import PhotoCard from "./PhotoCard";
+
+
+const TopGenerations =async () => {
+  const res = await fetch("https://pixgen-black-delta.vercel.app/data.json");
+  const photos = await res.json();
+  const topPhotos = photos.slice(0, 8);
+  console.log(topPhotos);
+  return (
+    <div>
+      <h2 className="text-2xl font-bold my-5">Top Generations</h2>
+
+      <div className="grid grid-cols-4 gap-5">
+        {
+          topPhotos.map(photo => 
+            <PhotoCard key={photo.id} photo={photo}/>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default TopGenerations;
